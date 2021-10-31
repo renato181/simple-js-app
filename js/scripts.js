75159fa521e2f90422373c05fcc1a6b941073730
renato181 <92191331+renato181@users.noreply.github.com>
2021-10-31 15:54:29 -0400

    made a pokemon list with three pokemon, with name, type, and hight

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood = 'chocolate';
-document.write(favoriteFood);
+let pokemonList = [{name: 'Bulbasaur', Type: ['grass', 'poison'], height: .7},
+  {name: 'Charmander', Type: 'fire', height: .6},
+  {name: 'Squirtle', Type: 'water', height: .5}
+];
