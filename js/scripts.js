facd2ccecfd4299b45aad2bd989659006229ee2c
renato181 <92191331+renato181@users.noreply.github.com>
2021-11-10 14:37:42 -0500

    changed the for loop to a forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,17 @@
 let pokemonList = [
-  {name: ' Bulbasaur ', Type: ['grass', 'poison'], height: .7},
-  {name: ' Ivysaur ', Type: ['grass', 'poison'], height: .99},
-  {name: ' Venusaur ', Type: ['grass', 'poison'], height: 2},
-  {name: ' Charmander ', Type: 'fire', height: .6},
-  {name: ' Charmeleon ', Type: 'fire', height: 1.09},
-  {name: ' Charizard ', Type: 'fire', height: 1.7},
-  {name: ' Squirtle ', Type: 'water', height: .5},
-  {name: ' Wartortle ', Type: 'water', height: .99},
-  {name: ' Blastoize ', Type: 'water', height: 1.6},
+  {name: ' Bulbasaur', type: [' grass', ' poison'], height: .7},
+  {name: ' Ivysaur', type: [' grass', ' poison'], height: .99},
+  {name: ' Venusaur', type: [' grass', ' poison'], height: 2},
+  {name: ' Charmander', type: ' fire', height: .6},
+  {name: ' Charmeleon', type: ' fire', height: 1.09},
+  {name: ' Charizard', type: ' fire', height: 1.7},
+  {name: ' Squirtle', type: ' water', height: .5},
+  {name: ' Wartortle', type: ' water', height: .99},
+  {name: ' Blastoize', type: ' water', height: 1.6},
 ];
 
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <0.85){
-    document.write(`<p> ${pokemonList[i].name} (height ${pokemonList[i].height} meters) is a small pokemon.</p><br>`);
-  }else if (pokemonList[i].height >1.85){
-    document.write(`<p> ${pokemonList[i].name} (height ${pokemonList[i].height} meters) Wow! That's big!</p><br>`);
-  }else {
-    document.write(`<p> ${pokemonList[i].name} (height ${pokemonList[i].height} meters) is an average pokemon.</p><br>`);
-  }
-}
+
+
+pokemonList.forEach((pokemon, i) => {
+  document.write('<p>' + pokemon.name + ' is a ' + pokemon.type + ' type' + ' standing at ' + pokemon.height + ' meters high</p>')
+});
